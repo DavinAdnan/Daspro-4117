8c7a8cc9f4ef2b4a86d3610de5bdc2214ffc2788
DavinAdnan <112920542+DavinAdnan@users.noreply.github.com>
2022-09-13 11:03:27 +0700

    Add files via upload
    Davin Adnan Rafidya 
    A11.2022.14649

---
+++ pertemuan3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Davin Adnan Rafidya\nA11.2022.14649\nSMAN 1 UNGARAN" << endl;
+
+    int Umur=18;
+    float tinggi=1.8;
+    char k='L';
+    string l="laki-laki";
+
+    cout << "\nUmur :" <<Umur;
+    cout << "\nTingggi :" <<tinggi;
+    cout << "\nKelamin :" <<k;
+    cout << "\nKelamin (string) :" <<l;
+    return 0;
+}
